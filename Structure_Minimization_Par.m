b675d23291a82196e64f3757bdd775c9ccea603e
Hayden Scheiber <scheiber@chem.ubc.ca>
2019-11-14 17:46:20 -0800

    Updated Parallel Structure Minimization Code
    Code now outputs data from all N structures in N x 10 Array

--- Structure_Minimization_Par.m
+++ Structure_Minimization_Par.m
@@ -6,6 +6,7 @@ function Output_Array = Structure_Minimization_Par(Salt,Model,Parameters,OptPos)
     
     N = length(Structures);
     Data_Array = cell(1,N);
+    Output_Array = nan(N,10);
     
     % Create a unique filename based on the current time for the log file
     filename = [char(datetime(now,'ConvertFrom','datenum','format','yyyy-MM-dd_HH.mm.ss')) ...
@@ -39,53 +40,42 @@ function Output_Array = Structure_Minimization_Par(Salt,Model,Parameters,OptPos)
     % Collect outputs and save diaries to single string variable
     Diary_Text = '';
     for idx = partitions
-        Data_Array{idx} = f(idx).OutputArguments;
+        Output_Array(idx,:) = f(idx).OutputArguments{1};
         Diary_Text = [Diary_Text f(idx).Diary newline newline]; %#ok<AGROW>
     end
     
     % Save diaries to text file    
     fid = fopen(filename,'wt');
-    fprintf(fid,Diary_Text');
+    encoded_str = unicode2native(Diary_Text, 'UTF-8');
+    fwrite(fid, encoded_str, 'uint8');
     fclose(fid);
     
-    % Determine if wurtzite has converged to 5-5
-    Wurtz_ind = strcmp(Structures,'Wurtzite');
-    if sum(Wurtz_ind) > 0
-        Wurtzite_Data = Data_Array{Wurtz_ind}{1};
-        delta_z = mod(Wurtzite_Data(7) - Wurtzite_Data(10),1);
-        if abs(delta_z-0.5) < 1e-2
-            Structures{Wurtz_ind} = 'FiveFive';
-        end
-    end
+%     % Determine if wurtzite has converged to 5-5
+%     Wurtz_ind = strcmp(Structures,'Wurtzite');
+%     if sum(Wurtz_ind) > 0 && OptPos
+%         Wurtzite_Data = Data_Array{Wurtz_ind}{1};
+%         delta_z = mod(Wurtzite_Data(7) - Wurtzite_Data(10),1);
+%         if abs(delta_z-0.5) < 1e-2
+%             Structures{Wurtz_ind} = 'FiveFive';
+%         end
+%     end
+%     
+%     % Determmine if BetaBeO has moved to rocksalt
+%     bBeO_ind = strcmp(Structures,'BetaBeO');
+%     if sum(bBeO_ind) > 0 && OptPos
+%         BetaBeO_Data = Data_Array{bBeO_ind}{1};
+%         a_minus_c = abs(BetaBeO_Data(2) - BetaBeO_Data(4)) < 1e-2;
+%         x1 = mod(BetaBeO_Data(5),1) - (1/4) < 1e-2;
+%         x2 = mod(BetaBeO_Data(8),1) - (1/4) < 1e-2;
+%         y1 = mod(BetaBeO_Data(6),1) - (3/4) < 1e-2;
+%         y2 = mod(BetaBeO_Data(9),1) - (3/4) < 1e-2;
+%         z1 = mod(BetaBeO_Data(7),1) < 1e-2;
+%         z2 = mod(BetaBeO_Data(10),1) < 1e-2;
+%         if a_minus_c && x1 && x2 && y1 && y2 && z1 && z2
+%             Structures{bBeO_ind} = 'Rocksalt';
+%         end
+%     end
+%     
     
-    % Determmine if BetaBeO has moved to rocksalt
-    bBeO_ind = strcmp(Structures,'BetaBeO');
-    if sum(bBeO_ind) > 0
-        BetaBeO_Data = Data_Array{bBeO_ind}{1};
-        a_minus_c = abs(BetaBeO_Data(2) - BetaBeO_Data(4)) < 1e-2;
-        x1 = mod(BetaBeO_Data(5),1) - (1/4) < 1e-2;
-        x2 = mod(BetaBeO_Data(8),1) - (1/4) < 1e-2;
-        y1 = mod(BetaBeO_Data(6),1) - (3/4) < 1e-2;
-        y2 = mod(BetaBeO_Data(9),1) - (3/4) < 1e-2;
-        z1 = mod(BetaBeO_Data(7),1) < 1e-2;
-        z2 = mod(BetaBeO_Data(10),1) < 1e-2;
-        if a_minus_c && x1 && x2 && y1 && y2 && z1 && z2
-            Structures{bBeO_ind} = 'Rocksalt';
-        end
-    end
-    
-    % Find lowest energy structure
-    Energies = nan(1,N);
-    for i = 1:N
-        Energies(i) = Data_Array{i}{1}(1);
-    end
-    [~,Ind] = min(Energies);
-    MinStruct = Structures{Ind};
-    
-    % Output data only if lowest energy structure is rocksalt
-    if strcmp(MinStruct,'Rocksalt')
-        Output_Array = Data_Array{Ind}{1};
-    else
-        Output_Array = nan(1,10);
-    end
+
 end
