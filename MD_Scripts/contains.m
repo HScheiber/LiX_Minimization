2f68c60aa90d56c50c353205838bc4a5df152641
Hayden Scheiber <scheiber@chem.ubc.ca>
2020-03-06 16:54:22 -0800

    Update contains.m

--- MD_Scripts/contains.m
+++ MD_Scripts/contains.m
@@ -1,8 +1,13 @@
-function output = contains(str,pattern)
-    match = regexp(str,pattern,'ONCE');
+function output = contains(str,pattern,extra,extra2)
+	if strcmpi(extra,'ignorecase') && extra2
+		match = regexpi(str,pattern,'ONCE');
+	else
+		match = regexp(str,pattern,'ONCE');
+	end
     if isempty(match)
         output = false;
     else
         output = true;
     end
+	
 end
